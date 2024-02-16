6d625c368aa363ce95c5878d32d641c0238deef7
“Will <wwh22@uw.edu>
2024-02-16 09:17:49 -0800

    Add contributors component

---
+++ .vitepress/theme/Contributors.vue
@@ -0,0 +1,96 @@
+<script>
+export default {
+  data() {
+    return {
+      contributors: []
+    };
+  },
+  mounted() {
+    fetch('/contributors.json')
+      .then(response => response.json())
+      .then(data => {
+        this.contributors = data;
+      });
+  },
+  computed: {
+    gridStyle() {
+      const columns = this.contributors.length;
+      return {
+        'grid-template-columns': `repeat(auto-fit, minmax(150px, 1fr))`,
+        'max-width': `${150 * columns}px`
+      };
+    }
+  }
+};
+</script>
+
+<template>
+  <div class="contributors-container">
+    <h2>Contributors</h2>
+    <div class="grid-container" :style="gridStyle">
+      <div v-for="contributor in contributors" :key="contributor.name" class="contributor">
+        <img :src="contributor.image" :alt="contributor.name" class="contributor-image">
+        <div class="contributor-name">{{ contributor.name }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+  
+<style>
+:root {
+  --text-color-light: rgba(60, 60, 67, 0.78);
+  /* Light mode text color */
+  --text-color-dark: #ffffff;
+  /* Dark mode text color */
+}
+
+.theme-default {
+  --text-color: var(--text-color-light);
+}
+
+.theme-default-dark {
+  --text-color: var(--text-color-dark);
+}
+
+.contributors-container {
+  text-align: center;
+  margin-top: 100px;
+}
+
+.grid-container {
+  display: grid;
+  gap: 10px;
+  padding: 10px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.contributor {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.contributors-container h2 {
+  font-size: 1.5em;
+  margin-bottom: 0.5em;
+  /* color: rgba(60, 60, 67, 0.78); */
+}
+
+.contributor-image {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+
+.contributor-name {
+  margin-top: 2px;
+  /* color: rgba(60, 60, 67, 0.78); */
+}
+
+.contributors-container h2,
+.contributor-name {
+  color: var(--text-color);
+}
+</style>
--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,4 +1,14 @@
+import { h } from "vue";
 import DefaultTheme from "vitepress/theme";
+import Contributors from "./Contributors.vue";
 import "./custom.css";
 
-export default DefaultTheme;
+/** @type {import('vitepress').Theme} */
+export default {
+  extends: DefaultTheme,
+  Layout: () => {
+    return h(DefaultTheme.Layout, null, {
+      "home-features-after": () => h(Contributors),
+    });
+  },
+};
